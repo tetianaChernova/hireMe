3c4f3dfc3f78a01148ae3aae201817332bb4836f
tetiana.chernova <>
2021-02-04 23:51:34 +0200

    initial commit

---
+++ src/test/java/com/chernova/hire/HireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chernova.hire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
